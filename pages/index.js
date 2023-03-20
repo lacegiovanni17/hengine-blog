90b8b6018d645409152396701d7ea5f7a2f6e147
Chidike Henry <dk1online25@gmail.com>
2023-03-20 22:48:22 +0100

    added new component

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,7 @@ export default function Home() {
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
+
           </div>
         </div>
       </div>
