ad47df8e33fd0716374093028a23dcd1d91586bf
Chidike Henry <dk1online25@gmail.com>
2023-03-20 22:44:19 +0100

    added new feat

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,34 @@
-import React from "react";
+/* eslint-disable react/jsx-key */
+import Head from "next/head";
 
-const HomePage = () => {
+const posts = [
+  { title: "React Testing", excerpt: "Learn React Testing" },
+  { title: "React with Tailwind", excerpt: "Learn React with Tailwind" }
+];
+
+export default function Home() {
   return (
-    <div className="text-3xl text-green-600 p-2">
-      Hello Geeks!
+    <div className="container mx-auto px-4 mb-8">
+      <Head>
+        <title>Hengine Blog</title>
+        <meta name="description" content="A blog about my thoughts" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <div>
+              {post.title}
+              {post.excerpt}
+            </div>
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+          </div>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
-export default HomePage;
