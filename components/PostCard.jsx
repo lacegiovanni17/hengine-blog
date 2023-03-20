9bcc3966e749328859f79cebc2f869043919f531
Chidike Henry <dk1online25@gmail.com>
2023-03-20 23:33:36 +0100

    added new components

---
+++ components/PostCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PostCard = ({ post }) => {
+  return (
+      <div>
+          {post.title}
+          {post.excerpt}
+      </div>
+  )
+}
+
+export default PostCard
