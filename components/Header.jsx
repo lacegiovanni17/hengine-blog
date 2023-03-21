b7b02fa5651be7db8d46454fa6aabb37c6608daf
Chidike Henry <dk1online25@gmail.com>
2023-03-21 17:28:27 +0100

    added new feat

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Link from "next/link";
-
-const categories = [
-  { name: "React", slug: "react" },
-  { name: "Web development", slug: "web-dev" },
-];
+import { getCategories } from "../services";
 
 const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
@@ -21,7 +25,7 @@ const Header = () => {
           {categories.map((category) => (
             <Link Key={category.slug} href={`/category/${category.slug}`}>
               <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
-                    {category.name}
+                {category.name}
               </span>
             </Link>
           ))}
