c34e757874121c29bf29bea004a5939d27fa9760
Chidike Henry <dk1online25@gmail.com>
2023-03-24 00:07:28 +0100

    added new feat

---
+++ components/FeaturedPostCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FeaturedPostCard = ({ post }) => {
+  return (
+      <div className="relative h-72">
+          FeaturedPostCard
+      </div>
+  )
+}
+
+export default FeaturedPostCard
