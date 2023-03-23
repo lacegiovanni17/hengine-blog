16070c896d07958b54aae646c2e39a45389a8d64
Chidike Henry <dk1online25@gmail.com>
2023-03-23 23:11:29 +0100

    added new feat

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -2,7 +2,17 @@ import React, { useState, useEffect } from 'react';
 import moment from 'moment'
 import parse from 'html-react-parser';
 
-const Comments = ({slug}) => {
+import { getComments } from '../services';
+
+const Comments = ({ slug }) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getComments(slug).then((result) => {
+      setComments(result);
+    });
+  }, []);
+
   return (
     <div>
       <h1>CommentsForm</h1>
