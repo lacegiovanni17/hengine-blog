c0f727902d5dbfe4c93567296c6f170a02f18815
Chidike Henry <dk1online25@gmail.com>
2023-03-24 00:20:58 +0100

    added new feat

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../components";
 import { getPosts } from '../services';
+import { FeaturedPosts } from '../sections';
 
 
 export default function Home({ posts }) {
@@ -12,6 +13,7 @@ export default function Home({ posts }) {
         <meta name="description" content="A blog about my thoughts" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
---
+++ sections/index.js
@@ -0,0 +1,3 @@
+
+export { default as FeaturedPosts } from "./FeaturedPosts";
+
