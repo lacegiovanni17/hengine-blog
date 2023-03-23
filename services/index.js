8521a81b8d399fdaa3bc7b6076b640d1f287d923
Chidike Henry <dk1online25@gmail.com>
2023-03-23 23:37:17 +0100

    added new feat

--- services/index.js
+++ services/index.js
@@ -143,3 +143,20 @@ export const submitComment = async (obj) => {
 
   return result.json();
 };
+
+export const getComments = async (slug) => {
+  const query = gql`
+    query GetComments($slug: String!) {
+      comments(where: { post: { slug: $slug } }) {
+        name
+        createdAt
+        comment
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.comments;
+};
+
